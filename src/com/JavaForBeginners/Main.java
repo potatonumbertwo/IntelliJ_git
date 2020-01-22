e300a034c48b5356e9460909006245bb288810a5
potatonumbertwo <lsj900605@gmail.com>
2020-01-22 16:09:02 -0500

    see if it works on git hub

---
+++ src/com/JavaForBeginners/Main.java
@@ -0,0 +1,14 @@
+package com.JavaForBeginners;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi hihihihi~");
+        int count = 0;   // Count the number within the range, init to 0
+        int sum = 0;
+        for (int i = 1; 1 <= 899; i++) {
+            sum += i;
+            ++i;
+        }
+    }
+}
