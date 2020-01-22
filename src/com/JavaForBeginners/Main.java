8f5051deeea8ece1f4623929989e8172345d8965
potatonumbertwo <lsj900605@gmail.com>
2020-01-22 16:15:41 -0500

    see if it works on git hub

--- src/com/JavaForBeginners/Main.java
+++ src/com/JavaForBeginners/Main.java
@@ -3,12 +3,15 @@ package com.JavaForBeginners;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi hihihihi~");
-        int count = 0;   // Count the number within the range, init to 0
+
+        // Count the number within the range, init to 0
+        int count = 0;
         int sum = 0;
-        for (int i = 1; 1 <= 899; i++) {
+
+        for (int i = 1; i <= 100; i++) {
             sum += i;
             ++i;
         }
+        System.out.println(sum);
     }
 }
